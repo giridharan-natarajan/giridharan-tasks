09fbca672fe3281069c0e1b4d5e84cdb95ac3461
giridharan-natarajan <101104721+giridharan-natarajan@users.noreply.github.com>
2022-03-16 17:58:24 +0530

    Add files via upload

--- taskpoly.cs
+++ taskpoly.cs
@@ -8,11 +8,27 @@ string usern=Console.ReadLine();
 Console.WriteLine("Password");
 string passw=Console.ReadLine();
 }
-public void userpass(string membership,string pin)
+public void userpass(int n1, int n2)
+{
+Console.WriteLine("MembershipId");
+Console.WriteLine(n1);
+Console.WriteLine("Pin1");
+Console.WriteLine(n2);
+ }
+public void userpass(double n1,int n2)
+{
+Console.WriteLine("Phone number");
+Console.WriteLine(n1);
+Console.WriteLine("Pin2");
+Console.WriteLine(n2);
+}
+
 
 public static void Main()
 {
 poly obj=new poly();
 obj.userpass();
+obj.userpass(12345,1234);
+obj.userpass(9176457407,1234);
 Console.WriteLine("validation done");
 }}
