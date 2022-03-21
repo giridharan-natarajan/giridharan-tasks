d0d7381ac1e8d2a8cbad717f39345596f7e3379d
giridharan-natarajan <101104721+giridharan-natarajan@users.noreply.github.com>
2022-03-21 16:51:22 +0530

    Add files via upload

---
+++ Task2103.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Task2103
+    {
+            public static void Main()
+
+            {
+                FileStream fs = new FileStream
+                        ("T:\\softura\\Employee.txt" ,
+                        FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                BinaryWriter bw = new BinaryWriter(fs);
+                BinaryWriter bw1= new BinaryWriter(fs);
+                BinaryWriter bw2= new BinaryWriter(fs);
+                BinaryWriter bw3= new BinaryWriter(fs);
+            bw.Write("Azwxs");
+            bw1.Write("Mani");
+            bw2.Write("23");
+            bw3.Write("Male");
+
+                bw.Flush();
+                fs.Close();
+                FileStream fs1 = new FileStream(@"T:\softura\Employee.txt", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs1);
+            BinaryReader br1 = new BinaryReader(fs1);
+            BinaryReader br2 = new BinaryReader(fs1);
+            BinaryReader br3 = new BinaryReader(fs1);
+
+
+            string str = br.ReadString();
+            string str1 = br1.ReadString();
+            string str2 = br2.ReadString();
+            string str3 = br3.ReadString();
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            fs1.Close();
+                Console.Read();
+            }
+
+
+        }
+    }
+
+
---
+++ Task21032.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Task21032
+    {
+        public static void Main()
+        {
+            FileStream fs = new FileStream("T:\\softura\\ Sweets.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Chocolava");
+            str.Add("choco mouse");
+            str.Add("Dokla");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("T:\\softura\\ Sweets.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"T:\softura\sweets.txt");
+
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+            Console.ReadLine();
+        }
+    }
+}
