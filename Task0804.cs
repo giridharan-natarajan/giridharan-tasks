be1f2cb4aec5b6b9b48d7db0949787e61c79e783
giridharan-natarajan <101104721+giridharan-natarajan@users.noreply.github.com>
2022-04-08 17:26:18 +0530

    Add files via upload

---
+++ Task0804.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LYNK
+{
+    class Task0804
+    {
+        static int numbergoat(int eyes,
+								int Legs)
+		{
+			int number = 0;
+			number = (Legs) -  (eyes-1);
+			number = number / 2;
+			return number;
+			
+
+		}
+
+	
+		public static void Main()
+		{
+			int eyes = 52, Legs = 80;
+			int Goat = numbergoat(eyes, Legs);
+			
+
+			Console.WriteLine("Goat = " +
+							Goat);
+			Console.WriteLine("Ducks = " +
+							(Goat-2));
+		}
+	}
+}
+
